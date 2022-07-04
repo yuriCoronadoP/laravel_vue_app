717079d86d28d81927bfe988d67cfe475faa7926
yuriCoronado <mtra.yuricoronado@gmail.com>
2022-07-04 18:37:21 -0500

    definicion de rutas

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('libro', App\Http\Controllers\LibroController::class)->only('index','store','update','show','destroy');
--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
 Route::get('/', function () {
-    return view('home');
-})->name('home');
+    return view('welcome');
+});
+
+Route::get('{any}', function () {
+    return view('app');
+})->where('any','.*');
