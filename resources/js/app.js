8258125bc9a20a95f1ffcac1181c0937eb54b3a8
yuriCoronado <mtra.yuricoronado@gmail.com>
2022-07-04 19:27:45 -0500

    configuracion de app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,29 @@
 require('./bootstrap');
+
+import vue, { h } from 'vue'
+window.Vue = vue
+
+import App from './components/App.vue'
+
+// importamos Axios
+import VueAxios from 'vue-axios';
+import axios from 'axios';
+
+// importamos y configuramos el vue-router
+import VueRouter from 'vue-router'
+import { routes } from './routes'
+import Vue from 'vue'
+Vue.use(VueRouter)
+Vue.use(VueAxios, axios)
+// creamos instancia de vueRouters
+const router = new VueRouter({
+    mode: 'history',
+    routes: routes
+})
+
+// creamos instancia de Vue
+const app = new Vue({
+    el: '#app',
+    router: router,
+    render: h => h(App)
+})
